c4ed325f2082dbe0130a28d16021afbcc340e4d0
default <ssumesh7650@gmail.com>
2024-09-10 14:31:23 +0000

    first module completed

--- python.py
+++ python.py
@@ -59,3 +59,11 @@ product = float_1 * float_2
 x=product
 big_string = " The product was " + str(x)
 print big_string
+
+skill_completed = "Python Syntax"
+exercises_completed = 13
+#The amount of points for each exercise may change, because points don't exist yet
+points_per_exercise = 5
+point_total = 100
+point_total += exercises_completed*points_per_exercise
+print " I got "+str(point_total)+" points! "
