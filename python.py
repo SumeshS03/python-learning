f97e65209d67ff75b858767e95641f94fefcd38e
default <ssumesh7650@gmail.com>
2024-09-13 07:51:52 +0000

    flatten items

--- python.py
+++ python.py
@@ -504,3 +504,13 @@ def join_strings(words):
   return result
 
 print join_strings(n)
+
+n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
+def flatten(lists):
+  results = []
+  for numbers in lists:
+    for number in numbers:
+      results.append(number)
+  return results
+
+print flatten(n)
