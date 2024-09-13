fcaba7d22bfb0b37af23f51ea5f7f155cc71bfed
default <ssumesh7650@gmail.com>
2024-09-13 07:35:10 +0000

    elements in lists

--- python.py
+++ python.py
@@ -485,3 +485,11 @@ students = [lloyd, alice, tyler]
 avg = get_class_average(students)
 print(avg)
 print(get_letter_grade(avg))
+
+n = [3, 5, 7]
+
+def double_list(x):
+  for i in range(0, len(x)):
+    x[i] = x[i] * 2
+  return x
+print double_list(n)
