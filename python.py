826cf0ccc461dfaecbf698b065c74d5e8fc5a690
default <ssumesh7650@gmail.com>
2024-09-16 02:59:21 +0000

    for list

--- python.py
+++ python.py
@@ -547,3 +547,12 @@ for char in phrase:
   else:
     print char,
 print
+
+numbers  = [7, 9, 12, 54, 99]
+
+print "This list contains: "
+
+for num in numbers:
+  print num
+for num in numbers:
+  print num ** 2
