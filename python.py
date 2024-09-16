0b5e91871766d3d12515e1bde8b16d5f9c70b73e
default <ssumesh7650@gmail.com>
2024-09-16 06:05:35 +0000

    check triangle

--- python.py
+++ python.py
@@ -646,3 +646,16 @@ panda = Animal("Chad", 7, True)
 print zebra.name, zebra.age, zebra.is_hungry
 print giraffe.name, giraffe.age, giraffe.is_hungry
 print panda.name, panda.age, panda.is_hungry
+
+class Triangle(object):
+  number_of_sides = 3
+  def __init__(self, angle1, angle2, angle3):
+    self.angle1 = angle1
+    self.angle2 = angle2
+    self.angle3 = angle3
+    
+  def check_angles(self):
+    if (self.angle1 + self.angle2 + self.angle3) == 180:
+      return True
+    else:
+      return False
