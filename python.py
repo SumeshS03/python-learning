b72c74ca355a0b5e1cf718769dc6f1d964dc12fe
default <ssumesh7650@gmail.com>
2024-09-16 03:02:09 +0000

    for else

--- python.py
+++ python.py
@@ -556,3 +556,13 @@ for num in numbers:
   print num
 for num in numbers:
   print num ** 2
+fruits = ['banana', 'apple', 'orange', 'tomato', 'pear', 'grape']
+
+print 'You have...'
+for f in fruits:
+  if f == 'tomato':
+    print 'A tomato is not a fruit!' 
+    break
+  print 'A', f
+else:
+  print 'A fine selection of fruits!'
