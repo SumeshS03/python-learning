32088245a40c6e631c48a00d30bf0c4bc9a00c00
default <ssumesh7650@gmail.com>
2024-09-30 05:55:38 +0000

    variables and datatype

--- pythoncode.py
+++ pythoncode.py
@@ -5,3 +5,15 @@ This is a comment
 written in
 more than just one line
 """
+
+x = 10
+y = "string"
+print(x)
+print(str(y))
+x = str(10)
+x = float(10)
+#datatype
+x = 10
+y = "string"
+print(type(x))
+print(type(y))
